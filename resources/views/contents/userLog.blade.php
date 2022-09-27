bea01bdc5fc5aed6492badd0a1df9398c3b14427
Md Tahsin Sakib <tahsin15-10624@diu.edu.bd>
2022-09-28 00:53:40 +0600

    new theme

--- resources/views/contents/userLog.blade.php
+++ resources/views/contents/userLog.blade.php
@@ -1,11 +1,8 @@
 @auth
 @if((Auth::user()->type == 'SuperUser'))
-<a href="{{ url('/dashboard') }}" class="me-2 text-info">Dashboard</a>
+<a href="{{ url('/dashboard') }}" class="nav-link text-success">Dashboard</a>
 @endif
-<a href="{{ url('/logout') }}" class="text-danger me-2">Logout</a>
+<a href="{{ url('/logout') }}" class="nav-link text-danger">Logout</a>
 @else
-<a href="{{ route('login') }}" class="me-2 text-info">Login</a>
-@if (Route::has('register'))
-<a href="{{ route('register') }}" class="me-2 text-dark">Register</a>
-@endif
+<a href="{{ route('login') }}" class="nav-link text-success">Login</a>
 @endauth
