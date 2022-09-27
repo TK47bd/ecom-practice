c8733dcfaabb7a9944caa200cec76bb42eb8ae6b
Md Tahsin Sakib <tahsin15-10624@diu.edu.bd>
2022-09-27 20:52:11 +0600

    parts of content

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -9,22 +9,23 @@ use App\Models\User;
 class RedirectController extends Controller
 {
 
-   public function CheckUser(){
-    if(Auth::check()){
-        $UserType = Auth::user()->type;
-        if($UserType == 'SuperUser'){
-            return view('dashboard');
-        }else{
+    public function CheckUser()
+    {
+        if (Auth::check()) {
+            $UserType = Auth::user()->type;
+            if ($UserType == 'SuperUser') {
+                return view('dashboard');
+            } else {
+                return view('welcome');
+            }
+        } else {
             return view('welcome');
         }
-    }else{
-        return view('welcome');
     }
-   }   
 
-   public function LogoutUser(){
-    Auth::logout();
-    return redirect('/login');
-   }
-
+    public function LogoutUser()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
 }
--- resources/views/contents/header.blade.php
+++ resources/views/contents/header.blade.php
@@ -8,14 +8,14 @@
         </div>
         <div class="col-lg-6">
             <div class="d-flex align-items-center justify-content-center">
-                <a href="{{ url('/home') }}" class="navbar-brand ms-lg-5">
+                <a href="{{ url('/') }}" class="navbar-brand ms-lg-5">
                     <h1 class="m-0 display-4 text-primary"><span class="text-secondary">Magic</span>WOrld</h1>
                 </a>
             </div>
         </div>
         <div class="col-lg-3">
             <div class="d-flex align-items-center justify-content-end">
-                <a class="btn btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
+                @include('contents.userLog')
                 <a class="btn btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
                 <a class="btn btn-primary btn-square rounded-circle me-2" href="#"><i
                         class="fab fa-linkedin-in"></i></a>
@@ -28,15 +28,20 @@
 
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-5">
-    <a href="{{ url('/home') }}" class="navbar-brand d-flex d-lg-none">
+    <a href="{{ url('/') }}" class="navbar-brand d-flex d-lg-none">
         <h1 class="m-0 display-4 text-secondary"><span class="text-white">Magic</span>WOrld</h1>
     </a>
+
+    <div class="d-flex d-lg-none">
+        @include('contents.userLog')
+    </div>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav mx-auto py-0">
-            <a href="{{ url('/home') }}" class="nav-item nav-link active">Home</a>
+            <a href="{{ url('/') }}" class="nav-item nav-link active">Home</a>
             <a href="about.html" class="nav-item nav-link">About</a>
             <a href="service.html" class="nav-item nav-link">Service</a>
             <a href="product.html" class="nav-item nav-link">Product</a>
@@ -51,21 +56,6 @@
                 </div>
             </div>
             <a href="contact.html" class="nav-item nav-link">Contact</a>
-
-            @if (Route::has('login'))
-            @auth
-            @if((Auth::user()->type == 'SuperUser'))
-            <a href="{{ url('/dashboard') }}" class="nav-item nav-link">Dashboard</a>
-            @endif
-            <a href="{{ url('/logout') }}" class="nav-item nav-link text-danger">Log Out</a>
-            @else
-            <a href="{{ route('login') }}" class="nav-item nav-link">Log in</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="nav-item nav-link">Register</a>
-            @endif
-            @endauth
-            @endif
         </div>
     </div>
 </nav>
